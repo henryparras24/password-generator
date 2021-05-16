9f609ac18b05f8ceaff366c01885eea316f74acf
Henry Parras <henry.parras@yahoo.com>
2021-05-16 01:59:24 -0700

    attempting functionality, if statements, prompts, and confirms

--- script.js
+++ script.js
@@ -3,6 +3,26 @@ var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 
+// Start funcionality! Begin with length
+var userchoice = prompt("How many characters would you like in your password?", "please enter a number");
+
+if (userchoice < 8) {
+    alert("Nope! Must be between 8-128");
+}
+if (userchoice > 128) {
+    alert("Nope! Must be between 8-128");
+}
+else { alert("Please continue");}
+
+// Which characters would they like to include?
+var userchoiceUC = confirm("Would you like to include uppercase?");
+
+var userchoiceLC = confirm("Would you like to include lowercase?");
+
+var userchoiceN = confirm("Would you like to include numbers?");
+
+var userchoiceSC = confirm("Would you like to include special characters?");
+
 
 
 // Assignment Code
